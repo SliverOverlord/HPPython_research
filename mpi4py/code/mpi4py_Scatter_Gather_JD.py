c0e2bf5236ffda0ad01b8095f64a9f6581a0adca
SliverOverlord <deniojo@mnstate.edu>
2019-08-05 15:18:45 -0500

    Worked on Scatter() Gather()

--- mpi4py/code/mpi4py_Scatter_Gather_JD.py
+++ mpi4py/code/mpi4py_Scatter_Gather_JD.py
@@ -1,6 +1,6 @@
 #Author: Joshua DeNio
 #Date: 7/31/2019
-#Description: This program demenstrates using Send() and Recv() using mpi4py
+#Description: This program demenstrates using Scatter() and Gather() using mpi4py
 
 from mpi4py import MPI
 import numpy
@@ -9,38 +9,44 @@ import numpy
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
-row = 4
-col = 4
-
-#initialize recData as an empty numpyarray of the correct size
-recData = numpy.empty(16, dtype = numpy.int64)
 
 def main():
-    #the master does its stuff first
+    
+    #initialize recData as an empty numpyarray of the correct size and sendData to null
+
+    sendData = None
+    recData = numpy.empty(16, dtype = numpy.int64)
+    
+    row = 4
+    col = 4
+    
+    #the master does its work first
     if rank == 0:
         #populate the array
         sendData = numpy.arange(16,dtype = numpy.int64)
+        print("The original array is: ",sendData)
         
+        #scatter the data
         sendData = comm.Scatter(sendData, root = 0)
         print("\n")
-        print("Data from processor ", rank, ": ", sendData)
-        print("\n")
+        print("Data Scatterred to nodes\n")
+        
     #the clients now do their part
     else:
         #receiving on the client nodes
-        
         recData = comm.Gather(recData, root = 0)
 
         print("\n")
         print("Data from processor ", rank, ": ", recData)
         print("\n")
+        
 #a function to print the matrix
 #def printMatrix(matrix):
     #getDigit = lambda: len(str(max(mat)))
     #maxDigit = getDigit()
     #j = 0
     #for i in range(1, row * col +1):
-        ##print("{0:{width}}".format(matrix[j], width = maxDigit), " ", end = "")
+        #print("{0:{width}}".format(matrix[j], width = maxDigit), " ", end = "")
         
         #if ((i%col == 0) or (i == row * col)):
             #print("")
