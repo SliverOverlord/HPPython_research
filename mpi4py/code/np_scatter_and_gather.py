9377c6623f7de55277ef97444477e1eb0d19087d
Heecheon <heecheon92@gmail.com>
2019-08-05 20:08:53 -0500

    slight modification.

--- mpi4py/code/np_scatter_and_gather.py
+++ mpi4py/code/np_scatter_and_gather.py
@@ -37,6 +37,8 @@ def main():
     RANK = COMM.Get_rank()
     SIZE = COMM.Get_size()
     mat = np.arange(1,25,dtype='i').reshape(6,4) #numpy array
+    #mat = np.zeros((6,4), dtype='i') #numpy array
+            
     local_mat = np.zeros((2,4), dtype='i') #container to receive data
     mat_gathered = np.zeros((6,4), dtype='i') #container to gather data
 
@@ -44,16 +46,20 @@ def main():
         print("Initial matrix at processor at: {}".format(RANK))
         print(mat)
         print("Scattering the matrix to all processor from {}".format(MASTER))
-    #COMM.Scatter(sendbuf, recvbuf, root)
-    COMM.Scatter([mat, 8, MPI.INT], local_mat, root=MASTER)
+    #COMM.Scatter(sendbuf, recvbuf, root) is
+    #COMM.Scatter([mat (data), 8 (count), MPI.INT (datatype)], [local_mat, 8, MPI.INT], root=MASTER)
+    #COMM.Scatter([mat, 8, MPI.INT], local_mat, root=MASTER)
+    COMM.Scatter(mat, local_mat, root=MASTER)
 
     print("The matrix has been scattered!")
     print("***********************************************")
     print("Local matrix at processor {}:".format(RANK))
     print(local_mat)
     print("***********************************************")
+    #time.sleep(1)
     print("***********************************************")
     #COMM.Gather(send_localbuf, recv_gatheredBuf, root)
+    #COMM.Gather([local_mat (data), 8 (count), MPI.INT (datatype)], [mat, 8, MPI.INT], root=MASTER)
     COMM.Gather([local_mat, 8, MPI.INT], [mat_gathered, 8, MPI.INT], root=MASTER)
     if RANK == MASTER:
         print("Gathering the local matrix to the Master Processor!")
