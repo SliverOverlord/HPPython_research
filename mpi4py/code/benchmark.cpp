9f47e1ff49eb4ab2e6a6b97cb59490eb0fdf0cff
SliverOverlord <deniojo@mnstate.edu>
2019-08-09 00:34:53 -0500

    Constants changed

--- mpi4py/code/benchmark.cpp
+++ mpi4py/code/benchmark.cpp
@@ -8,43 +8,46 @@
 
 using namespace std;
 
-void mat_mult(int mat1[100][100], int mat2[100][100], int output_mat[100][100]);
+
+const int rowCount = 100;
+const int colCount = 100;
+
+string txtFile = "100x100_matrix.txt";
+
+void mat_mult(int mat1[rowCount][colCount], int mat2[rowCount][colCount], int output_mat[rowCount][colCount]);
 
 int main(){
-    int list_mat[100][100];
-    int list_mat2[100][100];
-    int output_mat[100][100];
     
-    int currentTime;
+    int list_mat[rowCount][colCount];
+    int list_mat2[rowCount][colCount];
+    int output_mat[rowCount][colCount];
     
-    int rowCount = 0;
-    int colCount = 0;
     
-    ifstream inFile("100x100_matrix.txt");
+    ifstream inFile(txtFile);
     if (inFile.is_open()){
         
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i < rowCount; i++){
             //
-            for (int j =0; j < 100; j++){
+            for (int j =0; j < colCount; j++){
                 inFile >> list_mat[i][j];
             }
         }
     }
     inFile.close();
     
-    ifstream inFile2("100x100_matrix.txt");
+    ifstream inFile2(txtFile);
     if (inFile2.is_open()){
         
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i < rowCount; i++){
             //
-            for (int j =0; j < 100; j++){
+            for (int j =0; j < colCount; j++){
                 inFile2 >> list_mat[i][j];
             }
         }
     }
     inFile2.close();
     /*
-    for (int i = 0; i < 100**100; i++){
+    for (int i = 0; i < 100; i++){
         mat_mult(list_mat, list_mat2, output_mat);
     }
     */
@@ -53,26 +56,25 @@ int main(){
     return 0;
 }
 
-void mat_mult(int mat1[100][100], int mat2[100][100], int output_mat[100][100]){
+void mat_mult(int mat1[rowCount][colCount], int mat2[rowCount][colCount], int output_mat[rowCount][colCount]){
     
-    int row = 100;
-    int col = 100;
-    
+    //Start timer
     auto startTime = chrono::high_resolution_clock::now();
     
-    for (int r = 0; r < row; r++){
-        for (int c = 0; c < col; c++){
-            for (int r_iter = 0; r_iter < row; r_iter++){                
+    for (int r = 0; r < rowCount; r++){
+        for (int c = 0; c < colCount; c++){
+            for (int r_iter = 0; r_iter < rowCount; r_iter++){                
                 output_mat[r][c] += mat1[r][r_iter] * mat2[r_iter][c];
             }
         }
     }
     
+    //End timer
     auto endTime = chrono::high_resolution_clock::now();
     chrono::duration<double> timeElapsed = endTime - startTime;
     
     cout << "Matrix multiplication of two" << endl;
-    cout << "Integer matrix of type matrix[100][100]" << endl;
+    cout << "Integer matrix of type matrix[" << rowCount << "][" << colCount << "]" << endl;
     cout << "\n";
     
     cout << "The processing time is: ";
