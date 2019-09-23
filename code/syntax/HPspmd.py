2b6319e47c596ac3858a5634df53a97cf1f102e2
Heecheon Park <heecheon92@gmail.com>
2019-09-23 12:50:41 -0500

    little more grammar added.

--- code/syntax/HPspmd.py
+++ code/syntax/HPspmd.py
@@ -1,8 +1,46 @@
-import abc
+from mpi4py import MPI
+import numpy as np
 
-class HPspmd(abc.ABC):
-    pass
+#import abc
 
+class HPspmd:
+
+    # Are these class attributes or instance attributes?
+    COMM = None
+    RANK = None
+    SIZE = None
+
+    def procs(self):
+        COMM = MPI.COMM_WORLD
+        RANK = MPI.Get_rank()
+        SIZE = MPI.Get_size()
+
+    def getRank(self): return RANK
+    def getSize(self): return SIZE
+
+    def __repr__(self):
+        pass
+
+    @staticmethod
+    def on(p):
+        pass
+        
+    @staticmethod
+    def matmul(mat1, mat2, output_mat):
+        return np.matmul(mat1, mat2, output_mat)
+    
+    @staticmethod
+    def matrix(row, col, datatype):
+        return np.zeros((row, col), dtype=datatype)
+        
+    @staticmethod
+    def matrix_read(filename, datatype):
+        return np.loadtxt(filename, usecols=range(0, col), dtype=datatype)
+    
+
+#class HPspmd(abc.ABC):
+#    pass
+#
 # HPspmd Abstract Base Class
 # Use '@abc.abstrctmethod' decorator to define abstract functions.# i.e.
 #
